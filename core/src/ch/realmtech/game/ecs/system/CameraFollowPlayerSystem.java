6143d7042dabd25b7b0b5d8098ca7e546feecb7a
FabienChatton <fabien.chatton@studentfr.ch>
2023-04-02 11:43:46 +0200

    fix camera, maintenant centre le joueur

--- core/src/ch/realmtech/game/ecs/system/CameraFollowPlayerSystem.java
+++ core/src/ch/realmtech/game/ecs/system/CameraFollowPlayerSystem.java
@@ -1,6 +1,7 @@
 package ch.realmtech.game.ecs.system;
 
 import ch.realmtech.game.ecs.ECSEngine;
+import ch.realmtech.game.ecs.component.Box2dComponent;
 import ch.realmtech.game.ecs.component.PlayerComponent;
 import ch.realmtech.game.ecs.component.PossitionComponent;
 import com.badlogic.ashley.core.Entity;
@@ -14,6 +15,7 @@ public class CameraFollowPlayerSystem extends IteratingSystem {
         super(Family
                 .all(PlayerComponent.class)
                 .all(PossitionComponent.class)
+                .all(Box2dComponent.class)
                 .get(),7);
         this.gameCamera = gameCamera;
     }
@@ -21,7 +23,8 @@ public class CameraFollowPlayerSystem extends IteratingSystem {
     @Override
     protected void processEntity(Entity entity, float deltaTime) {
         PossitionComponent possitionComponent = ECSEngine.POSSITION_COMPONENT_MAPPER.get(entity);
-        gameCamera.position.x = possitionComponent.x;
-        gameCamera.position.y = possitionComponent.y;
+        Box2dComponent box2dComponent = ECSEngine.BOX2D_COMPONENT_MAPPER.get(entity);
+        gameCamera.position.x = possitionComponent.x + box2dComponent.widthWorld/2;
+        gameCamera.position.y = possitionComponent.y + box2dComponent.heightWorld/2;
     }
 }
