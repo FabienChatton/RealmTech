9210e40abec65ed8ad0808d877ddfdf141a3afb7
FabienChatton <fabien.chatton@studentfr.ch>
2023-08-13 15:56:16 +0200

    Optimisation mémoire des cellules

--- core/src/ch/realmtech/game/ecs/component/InfCellComponent.java
+++ core/src/ch/realmtech/game/ecs/component/InfCellComponent.java
@@ -1,32 +1,20 @@
 package ch.realmtech.game.ecs.component;
 
-import ch.realmtech.RealmTech;
 import ch.realmtech.game.registery.CellRegisterEntry;
 import com.artemis.PooledComponent;
-import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
-
-import static com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
 
 public class InfCellComponent extends PooledComponent {
     public byte innerPosX;
     public byte innerPosY;
-    public Cell cell;
-    public StaticTiledMapTile tile;
     public CellRegisterEntry cellRegisterEntry;
 
-    public InfCellComponent set(RealmTech context, byte innerPosX, byte innerPosY, CellRegisterEntry cellRegisterEntry) {
+    public InfCellComponent set(byte innerPosX, byte innerPosY, CellRegisterEntry cellRegisterEntry) {
         this.innerPosX = innerPosX;
         this.innerPosY = innerPosY;
         this.cellRegisterEntry = cellRegisterEntry;
-        if (cell != null && tile != null) {
-            tile.setTextureRegion(cellRegisterEntry.getTextureRegion(context));
-        } else {
-            cell = new Cell();
-            tile = new StaticTiledMapTile(cellRegisterEntry.getTextureRegion(context));
-            cell.setTile(tile);
-        }
         return this;
     }
+
     @Override
     protected void reset() {
         innerPosX = 0;
