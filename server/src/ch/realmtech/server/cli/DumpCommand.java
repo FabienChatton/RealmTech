136337036a3f955c5d9bd2be8c67802995710f22
fabienChatton <fabien.chatton@studentfr.ch>
2023-11-24 17:24:24 +0100

    début commande dump avec verbose hérité

--- server/src/ch/realmtech/server/cli/DumpCommand.java
+++ server/src/ch/realmtech/server/cli/DumpCommand.java
@@ -5,22 +5,34 @@ import picocli.CommandLine;
 
 import java.util.concurrent.Callable;
 
-import static picocli.CommandLine.Command;
-import static picocli.CommandLine.ParentCommand;
+import static picocli.CommandLine.*;
 
 @Command(name = "dump",  description = "master dump command, see subcommands for more dump details.\n" +
                                         "Each subcommands has a verbose option",
         subcommands = {
-                DumpChunksCommand.class,
-                DumpItemsCommand.class,
-                DumpCraftResultCommand.class,
-                DumpPlayersCommand.class,
+                //DumpChunksCommand.class,
+                //DumpItemsCommand.class,
+                //DumpCraftResultCommand.class,
+                //DumpPlayersCommand.class,
                 DumpInventoryCommand.class
         }
 )
 public class DumpCommand implements Callable<Integer> {
     @ParentCommand
     CommunMasterCommand masterCommand;
+    private int verboseLevel = 0;
+
+    @Option(names = {"-v", "--verbose"}, description = "Show more detail about. verbose can be multiple for ever more detail", scope = ScopeType.INHERIT)
+    private void setVerbose(boolean[] verbose) {
+        verboseLevel = verbose.length;
+    }
+
+    public void printlnVerbose(int verboseLevel, Object x) {
+        if (this.verboseLevel >= verboseLevel) {
+            masterCommand.output.println(x);
+        }
+    }
+
     @Override
     public Integer call() throws Exception {
         CommandLine commandLine = new CommandLine(this);
--- server/src/ch/realmtech/server/cli/DumpInventoryCommand.java
+++ server/src/ch/realmtech/server/cli/DumpInventoryCommand.java
@@ -13,15 +13,13 @@ import com.artemis.utils.IntBag;
 import java.util.Arrays;
 import java.util.concurrent.Callable;
 
-import static picocli.CommandLine.*;
+import static picocli.CommandLine.Command;
+import static picocli.CommandLine.ParentCommand;
 
 @Command(name="inventory", description = "dump all inventories")
 public class DumpInventoryCommand implements Callable<Integer> {
     @ParentCommand
     private DumpCommand dumpCommand;
-
-    @Option(names = {"-v", "--verbose"}, description = "Show more detail about inventory")
-    private boolean[] verbose;
 
     @Override
     public Integer call() throws Exception {
@@ -30,33 +28,28 @@ public class DumpInventoryCommand implements Callable<Integer> {
         ComponentMapper<InventoryComponent> mInventory = dumpCommand.masterCommand.getWorld().getMapper(InventoryComponent.class);
         ComponentMapper<UuidComponent> mUuid = dumpCommand.masterCommand.getWorld().getMapper(UuidComponent.class);
         ComponentMapper<ItemComponent> mItem = dumpCommand.masterCommand.getWorld().getMapper(ItemComponent.class);
-        if (verbose != null && verbose.length >= 1) {
-            for (int i = 0; i < inventoryEntities.size(); i++) {
-                int inventoryId = inventoryData[i];
-                UuidComponent uuidComponent = mUuid.get(inventoryId);
-                InventoryComponent inventoryComponent = mInventory.get(inventoryId);
-                dumpCommand.masterCommand.output.println(String.format("%s, %s", uuidComponent, inventoryComponent));
-                if (verbose.length >= 2) {
-                    StringBuilder stringBuilder = new StringBuilder();
-                    for (int s = 0; s < inventoryComponent.inventory.length; s++) {
-                        ItemComponent itemComponent = mItem.get(inventoryComponent.inventory[s][0]);
-                        if (itemComponent != null) {
-                            stringBuilder.append(String.format("%s %d", itemComponent.itemRegisterEntry, InventoryManager.tailleStack(inventoryComponent.inventory[s])));
-                            stringBuilder.append(" ");
-                        }
-                    }
-                    if (!stringBuilder.toString().isBlank()) {
-                        dumpCommand.masterCommand.output.println(stringBuilder);
-                    }
-                }
-                if (verbose.length >= 3) {
-                    SerializedApplicationBytes applicationInventoryBytes = dumpCommand.masterCommand.getSerializerManagerController().getInventorySerializerManager().encode(dumpCommand.masterCommand.getWorld(), inventoryComponent);
-                    dumpCommand.masterCommand.output.println(String.format("%s", Arrays.toString(applicationInventoryBytes.applicationBytes())));
+        for (int i = 0; i < inventoryEntities.size(); i++) {
+            int inventoryId = inventoryData[i];
+            UuidComponent uuidComponent = mUuid.get(inventoryId);
+            InventoryComponent inventoryComponent = mInventory.get(inventoryId);
+            dumpCommand.printlnVerbose(1, String.format("%s, %s", uuidComponent, inventoryComponent));
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int s = 0; s < inventoryComponent.inventory.length; s++) {
+                ItemComponent itemComponent = mItem.get(inventoryComponent.inventory[s][0]);
+                if (itemComponent != null) {
+                    stringBuilder.append(String.format("%s %d", itemComponent.itemRegisterEntry, InventoryManager.tailleStack(inventoryComponent.inventory[s])));
+                    stringBuilder.append(" ");
                 }
             }
+            if (!stringBuilder.toString().isBlank()) {
+                dumpCommand.printlnVerbose(2, stringBuilder);
+            }
+            SerializedApplicationBytes applicationInventoryBytes = dumpCommand.masterCommand.getSerializerManagerController().getInventorySerializerManager().encode(dumpCommand.masterCommand.getWorld(), inventoryComponent);
+            dumpCommand.printlnVerbose(3, String.format("%s", Arrays.toString(applicationInventoryBytes.applicationBytes())));
+
         }
-        if (inventoryEntities.isEmpty()) dumpCommand.masterCommand.output.println("no inventories loaded");
-        else dumpCommand.masterCommand.output.println("inventories count: " + inventoryEntities.size());
+        if (inventoryEntities.isEmpty()) dumpCommand.printlnVerbose(0, "no inventories loaded");
+        else dumpCommand.printlnVerbose(0, "inventories count: " + inventoryEntities.size());
         return 0;
     }
 }
