1fc72f5916c471babc4c6766cd07d9cf5b42d961
Fabien Chatton <fabirot0@outlook.com>
2024-02-07 15:43:55 +0100

    configuration de lancement du server en ligne de commande avec port

--- server/src/ch/realmtech/server/netty/ConnexionCommand.java
+++ server/src/ch/realmtech/server/netty/ConnexionCommand.java
@@ -1,23 +1,29 @@
 package ch.realmtech.server.netty;
 
+import ch.realmtech.server.level.worldGeneration.SeedGenerator;
+
 import java.util.concurrent.Callable;
 
 import static picocli.CommandLine.Command;
 import static picocli.CommandLine.Option;
 
-@Command(name = "connexionConfiguration", description = "La configuration pour la connection.", version = "0.1", mixinStandardHelpOptions = true)
+@Command(name = "connexionConfiguration", description = "Server launcher configuration ", version = "0.1", mixinStandardHelpOptions = true)
 public class ConnexionCommand implements Callable<ConnexionConfig> {
-    @Option(names = {"-p", "--port"}, description = "Le port", defaultValue = "25533")
+    @Option(names = {"-p", "--port"}, description = "The port to open.", defaultValue = "25533")
     private int port;
 
-    @Option(names = {"-s", "--saveName"}, description = "Quel carte sera chargée par le serveur", defaultValue = "default")
+    @Option(names = {"-s", "--saveName"}, description = "The name of the save to load. Use a none existence save name to create a new save", defaultValue = "default")
     private String saveName;
+
+    @Option(names = {"--seed"}, description = "The seed of the save. Only apply to newly created save.")
+    private long seed;
 
     @Override
     public ConnexionConfig call() throws Exception {
         return ConnexionConfig.builder()
                 .setSaveName(saveName)
                 .setPort(port)
+                .setSeed(seed != 0 ? seed : SeedGenerator.randomSeed())
                 .build();
     }
 }
