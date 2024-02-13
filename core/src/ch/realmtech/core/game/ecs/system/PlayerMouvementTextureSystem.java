b1db7a20bd064b5fa8f9d7b97ded30de65e9cf3c
FabienChatton <fabien.chatton@studentfr.ch>
2024-02-13 20:00:58 +0100

    animation joueur enfin fluide

--- core/src/ch/realmtech/core/game/ecs/system/PlayerMouvementTextureSystem.java
+++ core/src/ch/realmtech/core/game/ecs/system/PlayerMouvementTextureSystem.java
@@ -7,23 +7,51 @@ import ch.realmtech.server.ecs.plugin.SystemServerTickSlave;
 import com.artemis.ComponentMapper;
 import com.artemis.annotations.All;
 import com.artemis.systems.IteratingSystem;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SystemServerTickSlave
 @All(PlayerComponent.class)
 public class PlayerMouvementTextureSystem extends IteratingSystem {
     private ComponentMapper<PlayerComponent> mPlayer;
     private ComponentMapper<PositionComponent> mPos;
+    private List<Vector2> oldPoss;
+
+    @Override
+    protected void initialize() {
+        super.initialize();
+        oldPoss = new FixList<>(10);
+    }
+
     @Override
     protected void process(int entityId) {
         PlayerComponent playerComponent = mPlayer.get(entityId);
         PositionComponent positionComponent = mPos.get(entityId);
-        playerComponent.moveLeft = playerComponent.oldPos.x > positionComponent.x;
-        playerComponent.moveDown = playerComponent.oldPos.y > positionComponent.y;
-        playerComponent.moveUp = playerComponent.oldPos.y < positionComponent.y;
-        playerComponent.moveRight = playerComponent.oldPos.x < positionComponent.x;
 
-        if (!(playerComponent.oldPos.x == positionComponent.x && playerComponent.oldPos.y == positionComponent.y)) {
-            playerComponent.oldPos.set(positionComponent.x, positionComponent.y);
+        oldPoss.add(new Vector2(positionComponent.x, positionComponent.y));
+        playerComponent.moveLeft = oldPoss.get(0).x > positionComponent.x;
+        playerComponent.moveDown = oldPoss.get(0).y > positionComponent.y;
+        playerComponent.moveUp = oldPoss.get(0).y < positionComponent.y;
+        playerComponent.moveRight = oldPoss.get(0).x < positionComponent.x;
+
+    }
+}
+
+class FixList<T> extends ArrayList<T> {
+    private final int maxCapacity;
+
+    public FixList(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
+    }
+
+    @Override
+    public boolean add(T t) {
+        boolean ret = super.add(t);
+        if (size() > maxCapacity) {
+            removeRange(0, size() - maxCapacity);
         }
+        return ret;
     }
 }
