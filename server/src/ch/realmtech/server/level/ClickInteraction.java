f166aec3fb144f6ed9cd28638a3176033dfc8387
Fabien Chatton <fabirot0@outlook.com>
2024-03-25 13:54:36 +0100

    et click interaction

---
+++ server/src/ch/realmtech/server/level/ClickInteraction.java
@@ -0,0 +1,7 @@
+package ch.realmtech.server.level;
+
+import ch.realmtech.server.mod.ClientContext;
+
+public interface ClickInteraction {
+    void accept(ClientContext clientContext, int cellId);
+}
--- server/src/ch/realmtech/server/level/RightClickInteraction.java
+++
@@ -1,7 +0,0 @@
-package ch.realmtech.server.level;
-
-import ch.realmtech.server.mod.ClientContext;
-
-import java.util.function.BiConsumer;
-
-public interface RightClickInteraction extends BiConsumer<ClientContext, Integer> { }
