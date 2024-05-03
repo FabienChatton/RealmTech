2c7afbb36550a942512747dccb2254b910b802d2
Fabien Chatton <fabirot0@outlook.com>
2024-05-03 10:18:05 +0200

    fix: simulation furnace client, same calcule to match the server (#14)

--- core/src/ch/realmtech/core/game/ecs/system/FurnaceSimulationSystem.java
+++ core/src/ch/realmtech/core/game/ecs/system/FurnaceSimulationSystem.java
@@ -1,6 +1,8 @@
 package ch.realmtech.core.game.ecs.system;
 
 import ch.realmtech.core.game.ecs.component.FurnaceExtraInfoComponent;
+import ch.realmtech.core.game.ecs.plugin.SystemsAdminClient;
+import ch.realmtech.server.craft.CraftResult;
 import ch.realmtech.server.ecs.component.CraftingTableComponent;
 import ch.realmtech.server.ecs.component.FurnaceComponent;
 import ch.realmtech.server.ecs.component.InventoryComponent;
@@ -8,11 +10,16 @@ import ch.realmtech.server.ecs.component.ItemComponent;
 import ch.realmtech.server.ecs.plugin.SystemServerTickSlave;
 import com.artemis.ComponentMapper;
 import com.artemis.annotations.All;
+import com.artemis.annotations.Wire;
 import com.artemis.systems.IteratingSystem;
+
+import java.util.Optional;
 
 @SystemServerTickSlave
 @All({CraftingTableComponent.class, FurnaceComponent.class, FurnaceExtraInfoComponent.class})
 public class FurnaceSimulationSystem extends IteratingSystem {
+    @Wire
+    private SystemsAdminClient systemsAdminClient;
     private ComponentMapper<FurnaceComponent> mFurnace;
     private ComponentMapper<FurnaceExtraInfoComponent> mFurnaceExtraInfo;
     private ComponentMapper<CraftingTableComponent> mCraftingTable;
@@ -23,15 +30,30 @@ public class FurnaceSimulationSystem extends IteratingSystem {
         FurnaceComponent furnaceComponent = mFurnace.get(entityId);
         FurnaceExtraInfoComponent furnaceExtraInfoComponent = mFurnaceExtraInfo.get(entityId);
         CraftingTableComponent craftingTableComponent = mCraftingTable.get(entityId);
+        InventoryComponent craftingResultInventoryComponent = mInventory.get(craftingTableComponent.craftingResultInventory);
 
         if (furnaceComponent.remainingTickToBurn > 0) {
             furnaceComponent.remainingTickToBurn--;
         }
 
-        if (furnaceComponent.remainingTickToBurn > 0 && mItem.has(mInventory.get(craftingTableComponent.craftingInventory).inventory[0][0])) {
-            if (furnaceComponent.tickProcess < furnaceExtraInfoComponent.lastTickProcessFull) {
-                furnaceComponent.tickProcess++;
+        Optional<CraftResult> craftResultOpt = systemsAdminClient.getCraftingManager().getNewCraftResult(craftingTableComponent);
+        ItemComponent itemWitness = mItem.get(craftingResultInventoryComponent.inventory[0][0]);
+
+        if (craftResultOpt.isEmpty()) {
+            furnaceComponent.tickProcess = 0;
+            return;
+        }
+
+        CraftResult craftResult = craftResultOpt.get();
+        if (itemWitness != null) {
+            if (itemWitness.itemRegisterEntry != craftResult.getItemResult()) {
+                furnaceComponent.tickProcess = 0;
+                return;
             }
+        }
+
+        if (furnaceComponent.tickProcess < furnaceExtraInfoComponent.lastTickProcessFull) {
+            furnaceComponent.tickProcess++;
         } else {
             furnaceComponent.tickProcess = 0;
         }
